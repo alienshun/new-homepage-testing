80101ba1c89381da1c9d41cd0e360a66f47bdbad
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-11 01:11:54 +0800

    Integrate FullCalendar resources into SiteResources
    Added FULL_CALENDAR object to manage FullCalendar resources.

--- assets/js/Config/SiteResources.js
+++ assets/js/Config/SiteResources.js
@@ -3,6 +3,22 @@
 
   const A = './assets/';
 
+  const FULL_CALENDAR = {
+    styles: [
+      {
+        href: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css'
+      }
+    ],
+    scripts: [
+      {
+        src: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js'
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales-all.min.js'
+      }
+    ]
+  };
+
   window.SiteResources = {
     site: {
       title: 'Joker Chen',
@@ -10,6 +26,11 @@
         href: A + 'images/favicon.png',
         type: 'image/jpeg'
       }
+    },
+
+    navigation: {
+      defaultPage: 'resume',
+      pages: ['resume', 'schedule', 'social', 'toolkit', 'meditations']
     },
 
     external: {
@@ -25,20 +46,12 @@
         },
         {
           href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-        },
-        {
-          href: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css'
         }
       ],
 
-      scripts: [
-        {
-          src: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js'
-        },
-        {
-          src: 'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales-all.min.js'
-        }
-      ],
+      libraries: {
+        fullCalendar: FULL_CALENDAR
+      },
 
       analytics: [
         {
@@ -58,48 +71,16 @@
         A + 'css/cover.css'
       ],
 
-      pages: [
-        A + 'css/social/social.css',
-        A + 'css/toolkit/toolkit.css',
-        A + 'css/schedule/schedule.css',
-        A + 'css/life/meditations.css'
-      ],
-
       optional: {
         blog: A + 'css/blog/blog.css'
       }
     },
 
     scripts: {
-      modules: [
+      core: [
         A + 'js/Functions/Top-nav.js',
-        A + 'js/Functions/Cover.js'
-      ],
+        A + 'js/Functions/Cover.js',
 
-      content: [
-        A + 'js/Content/EN/about/resume_EN.js',
-        A + 'js/Content/ZH/about/resume_ZH.js',
-
-        A + 'js/Content/EN/life/meditations_EN.js',
-        A + 'js/Content/ZH/life/meditations_ZH.js',
-
-        A + 'js/Content/EN/social/social_EN.js',
-        A + 'js/Content/ZH/social/social_ZH.js',
-
-        A + 'js/Content/EN/toolkit/toolkit_EN.js',
-        A + 'js/Content/ZH/toolkit/toolkit_ZH.js',
-
-        A + 'js/Content/EN/schedule/schedule_EN.js',
-        A + 'js/Content/ZH/schedule/schedule_ZH.js'
-      ],
-
-      pageFunctions: [
-        A + 'js/Functions/social/Social.js',
-        A + 'js/Functions/toolkit/Toolkit.js',
-        A + 'js/Functions/schedule/Schedule.js'
-      ],
-
-      general: [
         A + 'js/Functions/general/Clock.js',
         A + 'js/Functions/general/Theme.js',
         A + 'js/Functions/general/Translate.js',
@@ -113,6 +94,76 @@
 
       optional: {
         blog: A + 'js/Functions/blog/Blog.js'
+      }
+    },
+
+    pages: {
+      resume: {
+        route: 'about',
+        domId: 'resume',
+        mountId: 'mount-resume',
+        styles: [],
+        scripts: [
+          A + 'js/Content/EN/about/resume_EN.js',
+          A + 'js/Content/ZH/about/resume_ZH.js'
+        ]
+      },
+
+      schedule: {
+        route: 'schedule',
+        domId: 'schedule',
+        mountId: 'mount-schedule',
+        styles: [
+          ...FULL_CALENDAR.styles,
+          A + 'css/schedule/schedule.css'
+        ],
+        scripts: [
+          ...FULL_CALENDAR.scripts,
+          A + 'js/Content/EN/schedule/schedule_EN.js',
+          A + 'js/Content/ZH/schedule/schedule_ZH.js',
+          A + 'js/Functions/schedule/Schedule.js'
+        ]
+      },
+
+      social: {
+        route: 'social',
+        domId: 'social',
+        mountId: 'mount-social',
+        styles: [
+          A + 'css/social/social.css'
+        ],
+        scripts: [
+          A + 'js/Content/EN/social/social_EN.js',
+          A + 'js/Content/ZH/social/social_ZH.js',
+          A + 'js/Functions/social/Social.js'
+        ]
+      },
+
+      toolkit: {
+        route: 'toolkit',
+        domId: 'toolkit',
+        mountId: 'mount-toolkit',
+        styles: [
+          A + 'css/toolkit/toolkit.css'
+        ],
+        scripts: [
+          A + 'js/Content/EN/toolkit/toolkit_EN.js',
+          A + 'js/Content/ZH/toolkit/toolkit_ZH.js',
+          A + 'js/Functions/toolkit/Toolkit.js'
+        ]
+      },
+
+      meditations: {
+        route: 'meditations',
+        domId: 'meditations',
+        mountId: 'mount-meditations',
+        styles: [
+          A + 'css/life/meditations.css'
+        ],
+        scripts: [
+          A + 'js/Content/EN/life/meditations_EN.js',
+          A + 'js/Content/ZH/life/meditations_ZH.js'
+        ]
       }
     },
 
