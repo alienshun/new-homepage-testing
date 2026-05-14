c07bac3b22c91f3f74e7fbb026d8084be61ce430
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 15:29:43 +0800

    Add activities moments for July 15, 2025

---
+++ assets/js/Content/EN/life/activities_moments_2025_07_15.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  const target = window.ACTIVITIES_MOMENTS_EN || {
+    ui: {},
+    moments: []
+  };
+
+  if (!Array.isArray(target.moments)) {
+    target.moments = [];
+  }
+
+  target.moments.push({
+    dateKey: '2025_07_15',
+    dateISO: '2025-07-15',
+    dateLabel: '2025.07.15',
+
+    title: 'Autographed Copy of Battle Through the Heavens',
+    location: '',
+
+    cover: './assets/images/life/activities_moments/2025_07_15/cover.jpg',
+
+    summary: '',
+
+    body: [],
+
+    gallery: [
+      './assets/images/life/activities_moments/2025_07_15/01.jpg',
+      './assets/images/life/activities_moments/2025_07_15/02.jpg'
+    ]
+  });
+
+  window.ACTIVITIES_MOMENTS_EN = target;
+})();
