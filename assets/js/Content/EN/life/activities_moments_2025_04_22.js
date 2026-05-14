b3d866b17df27e2c0c5061dba365d6bfeceda1de
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 13:45:42 +0800

    Add activities moments for April 22, 2025

---
+++ assets/js/Content/EN/life/activities_moments_2025_04_22.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  const target = window.ACTIVITIES_MOMENTS_EN || {
+    ui: {},
+    moments: []
+  };
+
+  if (!Array.isArray(target.moments)) {
+    target.moments = [];
+  }
+
+  target.moments.push({
+    dateKey: '2025_04_22',
+    dateISO: '2025-04-22',
+    dateLabel: '2025.04.22',
+
+    title: 'Second Scholarship at USTC',
+    location: 'Old North Gate, East Campus, University of Science and Technology of China',
+
+    cover: './assets/images/life/activities_moments/2025_04_22/cover.jpg',
+
+    summary: '',
+
+    body: [],
+
+    gallery: [
+      './assets/images/life/activities_moments/2025_04_22/01.jpg',
+      './assets/images/life/activities_moments/2025_04_22/02.jpg'
+    ]
+  });
+
+  window.ACTIVITIES_MOMENTS_EN = target;
+})();
