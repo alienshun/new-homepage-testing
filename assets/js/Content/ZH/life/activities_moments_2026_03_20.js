66d08e80553f5dbc0aa5c2522bc0f1e73e9643eb
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 16:48:38 +0800

    Add activities moments for March 20, 2026

---
+++ assets/js/Content/ZH/life/activities_moments_2026_03_20.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  const target = window.ACTIVITIES_MOMENTS_ZH || {
+    ui: {},
+    moments: []
+  };
+
+  if (!Array.isArray(target.moments)) {
+    target.moments = [];
+  }
+
+  target.moments.push({
+    dateKey: '2026_03_20',
+    dateISO: '2026-03-20',
+    dateLabel: '2026.03.20',
+
+    title: '科大的第三笔奖学金',
+    location: '中国科学技术大学 · 东区 · 老北门',
+
+    cover: './assets/images/life/activities_moments/2026_03_20/cover.jpg',
+
+    summary: '',
+
+    body: [],
+
+    gallery: [
+      './assets/images/life/activities_moments/2026_03_20/01.jpg',
+      './assets/images/life/activities_moments/2026_03_20/02.jpg'
+    ]
+  });
+
+  window.ACTIVITIES_MOMENTS_ZH = target;
+})();
