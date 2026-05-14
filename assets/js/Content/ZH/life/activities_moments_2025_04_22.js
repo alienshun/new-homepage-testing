aa12e31921752c4303e436612f93b967b419d2c3
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 13:46:04 +0800

    Add activities moments for April 22, 2025

---
+++ assets/js/Content/ZH/life/activities_moments_2025_04_22.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  const target = window.ACTIVITIES_MOMENTS_ZH || {
+    ui: {},
+    moments: []
+  };
+
+  if (!Array.isArray(target.moments)) {
+    target.moments = [];
+  }
+
+  target.moments.push({
+    dateKey: '2025_04_22',
+    dateISO: '2025-04-22',
+    dateLabel: '2025.04.22',
+
+    title: '科大的第二笔奖学金',
+    location: '中国科学技术大学东区老北门',
+
+    cover: './assets/images/life/activities_moments/2025_04_22/cover.jpg',
+
+    summary: '',
+
+    body: [],
+
+    gallery: [
+      './assets/images/life/activities_moments/2025_04_22/01.jpg',
+      './assets/images/life/activities_moments/2025_04_22/02.jpg'
+    ]
+  });
+
+  window.ACTIVITIES_MOMENTS_ZH = target;
+})();
