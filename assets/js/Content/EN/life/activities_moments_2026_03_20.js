fb0f1f1ddc5d82c9eed2a7f6e6feee4bc9ec49e7
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 16:48:12 +0800

    Add activities moments for March 20, 2026

---
+++ assets/js/Content/EN/life/activities_moments_2026_03_20.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  const target = window.ACTIVITIES_MOMENTS_EN || {
+    ui: {},
+    moments: []
+  };
+
+  if (!Array.isArray(target.moments)) {
+    target.moments = [];
+  }
+
+  target.moments.push({
+    dateKey: '2026_03_20',
+    dateISO: '2026-03-20',
+    dateLabel: '2026.03.20',
+
+    title: 'Third Scholarship at USTC',
+    location: 'Old North Gate, East Campus, University of Science and Technology of China',
+
+    cover: './assets/images/life/activities_moments/2026_03_20/cover.jpg',
+
+    summary: '',
+
+    body: [],
+
+    gallery: [
+      './assets/images/life/activities_moments/2026_03_20/01.jpg',
+      './assets/images/life/activities_moments/2026_03_20/02.jpg'
+    ]
+  });
+
+  window.ACTIVITIES_MOMENTS_EN = target;
+})();
