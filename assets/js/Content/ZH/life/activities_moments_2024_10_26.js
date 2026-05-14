2cebc27da61c86e7ea75c4d33b9c8bd1ca227981
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 13:39:53 +0800

    Add activities moments for 2024-10-26

---
+++ assets/js/Content/ZH/life/activities_moments_2024_10_26.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict';
+
+  const target = window.ACTIVITIES_MOMENTS_ZH || {
+    ui: {},
+    moments: []
+  };
+
+  if (!Array.isArray(target.moments)) {
+    target.moments = [];
+  }
+
+  target.moments.push({
+    dateKey: '2024_10_26',
+    dateISO: '2024-10-26',
+    dateLabel: '2024.10.26',
+
+    title: '书院团体拔河赛',
+    location: '中国科学技术大学中校区体育馆',
+
+    cover: './assets/images/life/activities_moments/2024_10_26/cover.jpg',
+
+    summary: '',
+
+    body: [],
+
+    gallery: [
+      './assets/images/life/activities_moments/2024_10_26/01.jpg',
+      './assets/images/life/activities_moments/2024_10_26/02.jpg',
+      './assets/images/life/activities_moments/2024_10_26/03.jpg',
+      './assets/images/life/activities_moments/2024_10_26/04.jpg'
+    ]
+  });
+
+  window.ACTIVITIES_MOMENTS_ZH = target;
+})();
