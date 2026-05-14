7238da532bebead9667c7302263d5f1834c906cd
Jinghao Chen <stardust.math26@Gmail.com>
2026-05-14 15:31:59 +0800

    Add titleParts for activities moment on 2025-07-15

--- assets/js/Content/EN/life/activities_moments_2025_07_15.js
+++ assets/js/Content/EN/life/activities_moments_2025_07_15.js
@@ -16,6 +16,10 @@
     dateLabel: '2025.07.15',
 
     title: 'Autographed Copy of Battle Through the Heavens',
+    titleParts: [
+      { text: 'Autographed Copy of ' },
+      { text: 'Battle Through the Heavens', italic: true }
+    ],
     location: '',
 
     cover: './assets/images/life/activities_moments/2025_07_15/cover.jpg',
