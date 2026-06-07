b735a5f9eb2551e0a962a6896a11d3999c562615
Jinghao Chen <stardust.math26@Gmail.com>
2026-06-08 01:17:11 +0800

    Add ORCID to Site Footer Label

--- assets/js/Functions/general/SiteFooterLabel.js
+++ assets/js/Functions/general/SiteFooterLabel.js
@@ -6,6 +6,8 @@
 
     repositoryApi: 'https://api.github.com/repos/Stardust-math/Stardust-math.github.io',
     githubProfile: 'https://github.com/Stardust-math',
+    orcidProfile: 'https://orcid.org/0009-0009-1961-6829',
+    orcidIcon: 'https://orcid.org/sites/default/files/images/orcid_16x16.png',
 
     copyright: 'Copyright © 2026 Jinghao Chen',
     quote: 'You have reached the end of this page, but not the end of the journey.',
@@ -228,10 +230,16 @@
       '</div>',
 
       '<div class="site-footer-label__right">',
-      `  <a class="site-footer-label__github-link" href="${CONFIG.githubProfile}" target="_blank" rel="noopener noreferrer" aria-label="Open GitHub profile">`,
-      `    ${githubIconSvg()}`,
-      '    <span>GitHub</span>',
-      '  </a>',
+      '  <div class="site-footer-label__links" aria-label="External profile links">',
+      `    <a class="site-footer-label__profile-link site-footer-label__github-link" href="${CONFIG.githubProfile}" target="_blank" rel="noopener noreferrer" aria-label="Open GitHub profile">`,
+      `      ${githubIconSvg()}`,
+      '      <span>GitHub</span>',
+      '    </a>',
+      `    <a class="site-footer-label__profile-link site-footer-label__orcid-link" href="${CONFIG.orcidProfile}" target="_blank" rel="me noopener noreferrer" aria-label="Open ORCID record 0009-0009-1961-6829">`,
+      `      <img class="site-footer-label__orcid-icon" src="${CONFIG.orcidIcon}" alt="ORCID iD icon" loading="lazy" decoding="async">`,
+      '      <span>ORCID</span>',
+      '    </a>',
+      '  </div>',
       '</div>'
     ].join('');
 
